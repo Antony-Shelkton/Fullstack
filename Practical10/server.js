1b9bd25e96b433f2ea2ba479788b4cc0350337b7
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2024-01-07 22:27:13 +0530

    Delete Practical10 directory

--- Practical10/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/',(req,res)=>{
-    res.send("Docker is Running ... ");
-});
-console.log("Server Started");
-app.listen(8000);
